3f2a6b8239a7c74943fde90fabd565cda272cae0
FMotalleb <fmotalleb@gmail.com>
2022-01-02 15:38:42 +0330

    added rgb and rgba input support needed for vscode extension

---
+++ lib/ui_related/color_methods/color_methods.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+Color rgb(int r, int g, int b) {
+  return Color.fromRGBO(r, g, b, 1);
+}
+
+Color rgba(int r, int g, int b, double o) {
+  return Color.fromRGBO(r, g, b, o);
+}
