cc32962b0ab44c1d85e6d50635970b0ca50d2da3
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2024-02-19 16:58:17 +0330

    export from visitor

--- lib/core/rust_like/option/helpers.dart
+++ lib/core/rust_like/option/helpers.dart
@@ -1,3 +1,4 @@
+import '../../contracts/typedefs/typedefs.dart';
 import 'option.dart';
 
 extension CheckAllOptionsExt on Iterable<Option<Object?>> {
@@ -33,4 +34,24 @@ extension WrapInOption<T> on T? {
 
 extension WrapInOptionS<T> on Iterable<T?> {
   Iterable<Option<T>> get opts => map(Option.wrap);
+}
+
+extension ExportFromVisitorExt<T, R extends T> on //
+    void Function(Adapter<T, bool>) {
+  /// Iterates through visitor and returns visited node of type [R]
+  /// that passed the [test] with true result returns [Some], otherwise
+  /// continues iteration and if no result where found returns [None]
+  Option<R> export(Adapter<T, bool> test) {
+    Option<R> result = None<R>();
+    this(
+      (item) {
+        if (item is R && test(item)) {
+          result = Some(item);
+          return false;
+        }
+        return true;
+      },
+    );
+    return result;
+  }
 }
